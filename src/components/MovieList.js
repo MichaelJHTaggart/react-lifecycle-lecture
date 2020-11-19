8c58e734373dbf7db334d544ea2ed91cf29e6477
Michael Taggart <mtaggarthawk@gmail.com>
2020-11-19 11:39:36 -0700

    lecture code complete! *kind of. I got 1 error

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -8,18 +8,36 @@ class MovieList extends Component {
     this.state = {
       isLoading: true,
       movies: [],
+      counter: 0
     }
+    this.handleCounterIncrement = this.handleCounterIncrement.bind(this)
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ movies: movies, isLoading: false })
+    }, 2500)
+  }
+
+  handleCounterIncrement() {
+    this.setState({ counter: this.state.counter + 1 })
   }
 
   render() {
     const mappedMovies = this.state.movies.map((element) => {
-      return <Movie movie={element} key={element.id} />
+      return <Movie
+        handleCounter Increment={this.handleCounterIncrement}
+        movie={element}
+        key={element.id}
+      />
     })
 
     return (
       <div className="movie-list">
         <h1>Here are some movies!</h1>
         {mappedMovies}
+        <h3>Counter: {this.state.counter}</h3>
+        {this.state.isLoading ? <h1>LOADING YOUR MOVIES...</h1> : mappedMovies}
       </div>
     )
   }
